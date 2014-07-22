1f661d621ffd433eb8a3aa1b569230fd24f3ab25
micha2718l <micha2718l@gmail.com>
2014-07-21 23:52:33 -0700

    Initial commit

---
+++ src/com/micha2718l/colorsynth1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.micha2718l.colorsynth1;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
